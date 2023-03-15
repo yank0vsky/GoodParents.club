406e7f14f54d9132a7848e5de6de5dbc9940a074
Mikhail Polykovskij <glader.ru@gmail.com>
2023-03-15 14:59:32 +0400

    Отправка кода для входа

--- authn/views/email.py
+++ authn/views/email.py
@@ -88,9 +88,9 @@ def email_login(request):
                                "Если совсем ничего не выйдет, напишите нам, попробуем помочь.",
                 }, status=404)
 
-            code = Code.create_for_user(user=user, recipient=user.email, length=settings.AUTH_CODE_LENGTH)
-            async_task(send_auth_email, user, code)
-            async_task(notify_user_auth, user, code)
+        code = Code.create_for_user(user=user, recipient=user.email, length=settings.AUTH_CODE_LENGTH)
+        async_task(send_auth_email, user, code)
+        async_task(notify_user_auth, user, code)
 
         return render(request, "auth/email.html", {
             "email": user.email,
